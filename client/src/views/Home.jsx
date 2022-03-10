65cfc05d24157fa039e89fc708a2e56479e3d00a
Braulio Mora <brauliocdc59@gmail.com>
2022-03-09 17:18:51 -0800

    created a new component for cypher set up and room join features

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -1,34 +1,19 @@
 import React, { useEffect, useState } from "react";
 import { useMutation, useQuery, useSubscription } from "@apollo/client";
 import { GET_ME, GET_CYPHERS } from "../utils/queries";
-import { ADD_CYPHER } from "../utils/mutations";
 import CypherMenu from "../components/CypherMenu";
 import ChatWindow from "../components/ChatWindow";
 import { CYPHER_ADDED } from "../utils/subscription";
-import AddUserModal from "../components/AddUserModal";
+import CypherSetUp from "../components/CypherSetUp";
+
 export default function componentName() {
   const [chatWindowData, setChatWindowData] = useState([]);
   const { loading, data } = useQuery(GET_ME);
   const { loading: cypherLoading, data: cypherData } = useQuery(GET_CYPHERS);
-  const [addCypher, { error }] = useMutation(ADD_CYPHER, {
-    update(cache, { data: { addCypher } }) {
-      const { cyphers } = cache.readQuery({ query: GET_CYPHERS });
-      cache.writeQuery({
-        query: GET_CYPHERS,
-        data: { cyphers: [...cyphers, addCypher] },
-      });
-    },
-  });
+
   const { loading: cypherAddedLoading, data: newCypherData } =
     useSubscription(CYPHER_ADDED);
   const { __typename, ...userData } = data?.me || {};
-  const onClick = async () => {
-    try {
-      const cypherCreated = await addCypher({ variables: { input: userData } });
-    } catch (err) {
-      console.log(err);
-    }
-  };
 
   useEffect(
     () => console.log("new cypher sub", newCypherData),
@@ -39,7 +24,7 @@ export default function componentName() {
     <>
       <div className="flex flex-row">
         <div className="float-left flex-col relative flex h-screen w-3/12 bg-white">
-          <div className="flex flex-col h-1/6 bg-white border-x-2 border-t-2 mt-1 ml-1 rounded-t border-black">
+          {/* <div className="flex flex-col h-1/6 bg-white border-x-2 border-t-2 mt-1 ml-1 rounded-t border-black">
             <h1 className=" text-center border-b-2 h-2/6 border-black text-black">
               Cyphers
             </h1>
@@ -54,7 +39,14 @@ export default function componentName() {
               </button>
               <AddUserModal />
             </div>
-          </div>
+          </div> */}
+          <CypherSetUp
+            userData={userData}
+            cypherLoading={cypherLoading}
+            cypherData={cypherData}
+            chatWindowData={chatWindowData}
+            setChatWindowData={setChatWindowData} 
+            />
           <CypherMenu
             userData={userData}
             cypherLoading={cypherLoading}
