bee3e41026ef81c63baf2d047cea4709a3723d55
ludie lambright <ludiefletcher@aol.com>
2022-03-09 21:40:59 -0800

    added styles

--- client/src/components/AddUserModal.jsx
+++ client/src/components/AddUserModal.jsx
@@ -14,11 +14,11 @@ export default function AddUserModal() {
   return (
     <>
       <button
-        className="mr-1 mb-1 py-2 px-4 bg-purple-600 hover:bg-purple-700 focus:ring-purple-500 focus:ring-offset-purple-200 text-white w-32 transition
+        className="bg-purple-600 hover:bg-purple-700 focus:ring-purple-500 focus:ring-offset-purple-200 text-white w-32 transition
         ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg"
         onClick={() => setShowModal(true)}
       >
-        Join Cypher with Id
+        Join Cypher
       </button>
       {showModal ? (
         <>
