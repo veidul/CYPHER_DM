cbcbfebeece1351427ac69fca542b8722ea5ceb4
Naji Dia <najimdia@gmail.com>
2022-03-10 21:57:16 -0800

    server fixes

--- server/server.js
+++ server/server.js
@@ -54,7 +54,7 @@ async function startApolloServer(typeDefs, resolvers) {
 
 
   if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../client")));
+    app.use(express.static(path.join(__dirname, "../client/dist")));
   } else {
     app.get("*", (req, res) => {
       res.sendFile(path.join(__dirname, "../client/index.html"));
